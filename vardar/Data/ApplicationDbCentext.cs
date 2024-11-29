3dfef3b1ebe372ad662d7183a45a4c045ddd3b15
Emir Vardar <emir.vardar38@gmail.com>
2024-11-29 21:33:58 +0300

    değişklik

---
+++ vardar/Data/ApplicationDbCentext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vardar.Models;
+
+namespace vardar.Data
+{
+    public class ApplicationDbCentext : DbContext //dbcontext entityframworkten gelir verileri yönetmek için kullanırız
+    {
+        public ApplicationDbCentext(DbContextOptions<ApplicationDbCentext> options) : base(options)//yapıcı metot tanımlıyoruz
+        {//yapılan değişikler dbcontexte gönderilir
+            
+        }
+        public DbSet<Category> Categories { get; set; }//databasede yaracağımız table
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Aksiyon",DisplayOrder=1},
+                new Category { Id = 2, Name = "Bilim Kurgu", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Tarih", DisplayOrder = 3 }
+
+                );
+        }
+    }
+}
